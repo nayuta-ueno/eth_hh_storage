6b12e4e5eb982296e2ae4a8a374546735b76f03b
ueno <ueno@nayuta.co>
2021-01-30 23:41:02 +0900

    second

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -6,11 +6,13 @@ contract Storage {
     uint8 public v8_1;
     uint16 public v16_1;
     uint8 public v8_2;
+    mapping(string => uint256) vmap_1;
 
     constructor() public {
         v256_1 = uint256(-1);
         v8_1 = 0x56;
         v16_1 = 0x1234;
         v8_2 = 0x78;
+        vmap_1["abc"] = 100;
     }
 }
