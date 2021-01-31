5350be29fb9f4ee31ef8121e9a0fe2587215fd15
ueno <ueno@nayuta.co>
2021-01-31 13:42:13 +0900

    fourth

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -6,7 +6,13 @@ contract Storage {
     uint8 public v8_1;
     uint16 public v16_1;
     uint8 public v8_2;
-    mapping(string => uint256) vmap_1;
+    mapping(string => uint256) public vmap_1;
+    mapping(uint256 => string) public vmap_2;
+    uint32[] public arr_1;
+    string private str_1;
+    string private str_2;
+    bytes public byt_1;
+    bytes private byt_2;
 
     constructor() public {
         v256_1 = uint256(-1);
@@ -14,5 +20,19 @@ contract Storage {
         v16_1 = 0x1234;
         v8_2 = 0x78;
         vmap_1["abc"] = 100;
+        vmap_2[100] = "abc";
+        arr_1.push(1);
+        arr_1.push(2);
+        arr_1.push(3);
+        arr_1.push(4);
+        arr_1.push(5);
+        arr_1.push(6);
+        arr_1.push(7);
+        arr_1.push(8);
+        arr_1.push(9);
+        str_1 = "12345678901234567890123456789a";
+        str_2 = "12345678901234567890123456789012";
+        byt_1 = bytes("abcdefghijABCDEFGHIJabcdefghijk");
+        byt_2 = bytes("abcdefghijABCDEFGHIJabcdefghijkl");
     }
 }
--- test/TestStorage.js
+++ test/TestStorage.js
@@ -4,13 +4,70 @@ contract("test", async accounts => {
     it("init", async () => {
         const storage = await Storage.new();
 
-        for (let lp = 0; lp < 5; lp++) {
+        for (let lp = 0; lp < 4; lp++) {
             let st = await web3.eth.getStorageAt(storage.address, lp);
             console.log(`[${lp}] ${st}`);
         }
+        console.log();
 
-        let pos = web3.utils.soliditySha3("abc", 2);
+        // vmap_1
+        let index = 2;
+        let pos = web3.utils.soliditySha3("abc", index);
         let st = await web3.eth.getStorageAt(storage.address, pos);
-        console.log(`[${pos}] ${st}`);
+        console.log(`[vmap_1] ${st}`);
+        console.log();
+
+        // vmap_2
+        index = 3;
+        pos = web3.utils.soliditySha3(100, index);
+        st = await web3.eth.getStorageAt(storage.address, pos);
+        console.log(`[vmap_2] ${st}`);
+        console.log();
+
+        // arr_1
+        index = 4;
+        st = await web3.eth.getStorageAt(storage.address, index);
+        console.log(`[arr_1 len] ${st}`);
+
+        pos = web3.utils.soliditySha3(index);
+        st = await web3.eth.getStorageAt(storage.address, pos);
+        console.log(`[arr_1 0-7] ${st}`);
+
+        pos = web3.utils.toBN(pos).add(new web3.utils.BN('1'));
+        st = await web3.eth.getStorageAt(storage.address, pos);
+        console.log(`[arr_1 8] ${st}`);
+        console.log();
+
+        // str_1
+        index = 5;
+        st = await web3.eth.getStorageAt(storage.address, index);
+        console.log(`[str_1] ${st}`);
+        console.log();
+
+        // str_2
+        index = 6;
+        st = await web3.eth.getStorageAt(storage.address, index);
+        console.log(`[str_2 len] ${st}`);
+
+        pos = web3.utils.soliditySha3(index);
+        st = await web3.eth.getStorageAt(storage.address, pos);
+        console.log(`[str_2 0-31] ${st}`);
+        console.log();
+
+        // byt_1
+        index = 7;
+        st = await web3.eth.getStorageAt(storage.address, index);
+        console.log(`[byt_1] ${st}`);
+        console.log();
+
+        // byt_2
+        index = 8;
+        st = await web3.eth.getStorageAt(storage.address, index);
+        console.log(`[byt_2 len] ${st}`);
+
+        pos = web3.utils.soliditySha3(index);
+        st = await web3.eth.getStorageAt(storage.address, pos);
+        console.log(`[byt_2 0-31] ${st}`);
+        console.log();
     });
 });
